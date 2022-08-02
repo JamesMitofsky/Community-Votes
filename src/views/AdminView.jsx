4281f2de66358d06ee376ad42f50675be3c89e42
James Tedesco <JamesTedesco802@gmail.com>
2022-08-02 19:50:18 +0200

    Add in basic outline materials for voter view

--- src/views/AdminView.jsx
+++ src/views/AdminView.jsx
@@ -1,5 +1,4 @@
 import BuildVotable from "../components/BuildVotable.jsx";
-import { Container } from "@mui/material";
 
 export default function AdminView() {
   return <BuildVotable />;
--- src/views/VoterView.jsx
+++ src/views/VoterView.jsx
@@ -1,6 +1,25 @@
-import BuildVotable from "../components/BuildVotable.jsx";
-import { Container } from "@mui/material";
+import NameList from "../components/NameList.jsx";
+import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
 
 export default function VoterView() {
-  return <h1>Welcome, dear voter!</h1>;
+  // UPDATE: retrieve voter name via their id found in the URL
+  const voterName = "James";
+
+  //   UPDATE: get possible candidates from votable id
+  const names = [
+    { name: "James", id: uuidv4() },
+    { name: "Greg", id: uuidv4() },
+  ];
+
+  //   UPDATE: get available votes from votable info
+  const [availableVotes, setAvailableVotes] = useState(3);
+
+  return (
+    <>
+      <h1>Welcome to your ballot, {voterName} 👋</h1>
+      <h2>You have {availableVotes} votes available</h2>
+      <NameList names={names} />
+    </>
+  );
 }
