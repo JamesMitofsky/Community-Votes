cb08bafccd6487d81886d50408e19d696fcbade8
James Tedesco <JamesTedesco802@gmail.com>
2022-08-03 16:54:48 +0200

    Add friendly image to 404

--- src/views/UnknownAddress.jsx
+++ src/views/UnknownAddress.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import signsSVG from "../assets/signs.svg";
 
 export default function UnkownAddress() {
   return (
-    <h1>
-      Whoops, sorry about that! Would you like to return{" "}
-      <Link to="/">home</Link>?
-    </h1>
+    <>
+      <h1>
+        Whoops, sorry about that! Would you like to return{" "}
+        <Link to="/">home</Link>?
+      </h1>
+      <img style={{ width: "inherit" }} src={signsSVG} alt="" />
+    </>
   );
 }
