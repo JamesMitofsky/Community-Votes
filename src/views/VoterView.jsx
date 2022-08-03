281ab6cc2a370e87a5ee1234a3000ed2fae2a348
James Tedesco <JamesTedesco802@gmail.com>
2022-08-03 10:56:24 +0200

    Collect live votable data from server and implement in voter view

--- src/views/VoterView.jsx
+++ src/views/VoterView.jsx
@@ -1,25 +1,43 @@
 import NameList from "../components/NameList.jsx";
 import { v4 as uuidv4 } from "uuid";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function VoterView() {
-  // UPDATE: retrieve voter name via their id found in the URL
-  const voterName = "James";
+  // REQUIREMENT: get info from url
+  const urlParams = {
+    voterName: "James",
+    availableVotes: 3,
+  };
 
-  //   UPDATE: get possible candidates from votable id
-  const names = [
-    { name: "James", id: uuidv4() },
-    { name: "Greg", id: uuidv4() },
-  ];
+  // create states waiting to accept values
+  const [candidates, setCandidates] = useState([]);
+  const [availableVotes, setAvailableVotes] = useState(
+    urlParams.availableVotes
+  );
 
-  //   UPDATE: get available votes from votable info
-  const [availableVotes, setAvailableVotes] = useState(3);
+  useEffect(() => {
+    // get votable from server
+    const instance = axios.create({
+      baseURL: "http://127.0.0.1:5000",
+    });
+
+    // REQUIREMENT: get id from url
+    const votableID = "Test votableYgFjbbFmbZ";
+    instance(`/votables/${votableID}`, {
+      method: "get",
+      responseType: "stream",
+    }).then(function (response) {
+      const candidateArray = response.data.candidates;
+      setCandidates(candidateArray);
+    });
+  }, []);
 
   return (
     <>
-      <h1>Welcome to your ballot, {voterName} 👋</h1>
+      <h1>Welcome to your ballot, {urlParams.voterName} 👋</h1>
       <h2>You have {availableVotes} votes available</h2>
-      <NameList names={names} />
+      <NameList people={candidates} />
     </>
   );
 }
