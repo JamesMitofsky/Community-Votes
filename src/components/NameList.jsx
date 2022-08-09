3a0a92bf2291c32d592a3212db3c25e3f9f2ed5c
James Tedesco <JamesTedesco802@gmail.com>
2022-08-09 20:51:11 +0200

    Verify if votes property exists on the incoming array of people and then display votes if possible

--- src/components/NameList.jsx
+++ src/components/NameList.jsx
@@ -1,10 +1,21 @@
 import { List, ListItem, ListItemText } from "@mui/material";
 
 export default function NameList({ people }) {
+  // verify if people variable are candidates
+  const hasVotes = people[0]?.votes ? true : false;
+
+  // if candidates, sort by votes
+  if (hasVotes) {
+    people.sort((a, b) => b.votes - a.votes);
+  }
+
   const listItems = people.map((person) => {
     return (
       <ListItem divider key={person.id}>
-        <ListItemText primary={person.name} />
+        <ListItemText
+          primary={person.name}
+          secondary={hasVotes ? person.votes : ""}
+        />
       </ListItem>
     );
   });
