ced832b0fdf60ee46b115d405e414b7d76da053b
James Tedesco <JamesTedesco802@gmail.com>
2022-08-13 23:33:36 +0200

    Add loading state to results (this should be updated to respect the fact null IDs could be submitted and return nothing from the server

--- src/views/ResultsView.jsx
+++ src/views/ResultsView.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import NameList from "../components/NameList.jsx";
+import LoadingSpinner from "../components/LoadingSpinner.jsx";
 
 export default function ResultsView() {
   // const [candidateElms, setCandidateElms] = useState([]);
@@ -36,22 +37,40 @@ export default function ResultsView() {
 
         // create list elements of all candidate votes
         setCandidates(candidates);
+        setPageLoaded(true);
       })
       .catch(function (error) {
         console.log(error);
       });
   }, [urlParams.votableID]);
 
-  return (
+  const [pageLoaded, setPageLoaded] = useState(false);
+  const votableID = urlParams.votableID;
+
+  const mainView = (
     <>
-      <Helmet>
-        <title>Results</title>
-      </Helmet>
       <Typography variant="h1">Results for {urlParams.votableID}</Typography>
       {candidates.length > 0 ? (
         <NameList people={candidates} />
       ) : (
         <Typography variant="h3">No votes have been cast.</Typography>
+      )}
+    </>
+  );
+
+  return (
+    <>
+      <Helmet>
+        <title>Results</title>
+      </Helmet>
+      {/* If votable ID is coming but the page hasn't loaded, show the spinner */}
+      {votableID && !pageLoaded ? (
+        <LoadingSpinner />
+      ) : // If page has loaded, show the main view. If not loaded but there is no ID, print text.
+      pageLoaded ? (
+        mainView
+      ) : (
+        'There are no results to view. Please make sure there is a "votableID" parameter in the link you used.'
       )}
     </>
   );
