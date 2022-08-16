bcdd2097d6646bee0569db4b45abb20e6f3105c6
James Mitofsky <JamesMitofsky@gmail.com>
2022-08-16 13:42:55 +0200

    Remove error object from email, replacing the text with simplified explanations of the problem being experienced

--- src/components/alerts/Error.jsx
+++ src/components/alerts/Error.jsx
@@ -1,26 +1,21 @@
 import { Alert, AlertTitle, Snackbar } from "@mui/material";
 
-export default function Error({ state, response }) {
+export default function Error({ state, response, message }) {
   const email = {
     recipient: "jamesmitofsky@gmail.com",
     subject: "HOA Votes: something's not working right",
     body: `Can you tell us what you were trying to do when you received this error? Please type directly beneath this text, and we'll get back to you ASAP!`,
-    newlineSpace: "%0D%0A%0D%0A%0D%0A%0D%0A------------------",
-    serverResponse: JSON.stringify(response),
+    newlineSpace: "%0D%0A%0D%0A%0D%0A%0D%0A------------------%0D%0A",
   };
 
-  const constructedHref = `mailto:${email.recipient}?subject=${
-    email.subject
-  }&body=${email.body}${
-    email.serverResponse.length > 2 ? email.newlineSpace : ""
-  }${email.serverResponse.length > 2 ? email.serverResponse : ""}`;
+  const constructedHref = `mailto:${email.recipient}?subject=${email.subject}&body=${email.body}`;
   return (
     <Snackbar open={state} autoHideDuration={6000}>
       <Alert severity="error" sx={{ width: "100%" }}>
         <AlertTitle>Sorry about that, we had a problem. 👷</AlertTitle>
-        Help us get back on track by clicking{" "}
+        {message} Help us get back on track:{" "}
         <a rel="noreferrer" target="_blank" href={constructedHref}>
-          here
+          send an email
         </a>
         !
       </Alert>
