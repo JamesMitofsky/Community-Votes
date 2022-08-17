16227734393c6368aae6e1a14bc09f4ccce69b64
James Mitofsky <JamesMitofsky@gmail.com>
2022-08-17 15:26:40 +0200

    Replace modal (low level component) with a dialog from the MUI library

--- src/components/VotableConfirmation.jsx
+++ src/components/VotableConfirmation.jsx
@@ -1,12 +1,32 @@
+import { useState, forwardRef } from "react";
 import {
-  Box,
+  Dialog,
   Button,
-  Modal,
+  Container,
+  AppBar,
+  Toolbar,
+  IconButton,
   Typography,
-  Card,
-  CardContent,
+  Slide,
 } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 import NameList from "./NameList";
+
+// import  from "@mui/material/Dialog";
+// import  from "@mui/material/ListItemText";
+// import  from "@mui/material/ListItem";
+// import  from "@mui/material/List";
+// import  from "@mui/material/Divider";
+// import  from "@mui/material/AppBar";
+// import  from "@mui/material/Toolbar";
+// import  from "@mui/material/IconButton";
+// import  from "@mui/material/Typography";
+
+// import  from "@mui/material/Slide";
+
+const Transition = forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 export default function VotableConfirmation({
   isOpen,
@@ -15,46 +35,56 @@ export default function VotableConfirmation({
 }) {
   const { candidates, name, voters, id } = votableData.data;
 
+  const [open, setOpen] = useState(isOpen);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <>
-      <Modal
-        sx={{ padding: "2rem" }}
-        open={isOpen}
-        onClose={handleOpen}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+    <div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Open full-screen dialog
+      </Button>
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
       >
-        <Card sx={{ height: "100%" }}>
-          <CardContent>
-            <Typography id="modal-modal-title" variant="h2">
-              ✅{" "}
-              <Box
-                component="span"
-                style={{ textDecoration: "underline" }}
-                class="underline"
-              >
-                {name}
-              </Box>{" "}
-              was successfully created!
+        <AppBar sx={{ position: "relative" }}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h2">
+              Success! {name} has been created.
             </Typography>
-            <Typography variant="h3">
-              {candidates.length} Candidate{candidates.length > 1 ? "s" : "s"}{" "}
-            </Typography>
-            <NameList people={candidates} />
-            <Typography variant="h3">
-              {voters.length} Voter{voters.length > 1 ? "s" : "s"}
-            </Typography>
-            <NameList people={voters} />
-            <Typography variant="h3">Votable ID</Typography>
-            <Typography variant="p" sx={{ display: "block" }}>
-              {id}
-            </Typography>
-            <Button sx={{ mt: 5 }} variant="contained" onClick={handleOpen}>
-              Okay
+            <Button autoFocus color="inherit" onClick={handleClose}>
+              Close
             </Button>
-          </CardContent>
-        </Card>
-      </Modal>
-    </>
+          </Toolbar>
+        </AppBar>
+        <Container sx={{ mt: 4 }}>
+          <Typography variant="h3">
+            Added {candidates.length} Candidate{candidates.length > 1 && "s"}
+          </Typography>
+          <NameList people={candidates} />
+          <Typography variant="h3">
+            Added {voters.length} Voter{voters.length > 1 && "s"}
+          </Typography>
+          <NameList people={voters} />
+        </Container>
+      </Dialog>
+    </div>
   );
 }
