a8705df4720361aa8329bf7955665d727d065761
James Mitofsky <JamesMitofsky@gmail.com>
2022-08-17 18:41:34 +0200

    Quick fix for randomization. There were some Stack Overflow posts which indiciated this was biased, so it could be nice to revisit this.

--- src/components/VoterForm.jsx
+++ src/components/VoterForm.jsx
@@ -11,15 +11,7 @@ export default function VoterForm({
   castBallot,
 }) {
   function shuffleArray(array) {
-    const randomizedArray = array.map((el, i) => {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    });
-    return randomizedArray;
-    // for (let i = array.length - 1; i > 0; i--) {
-    //   const j = Math.floor(Math.random() * (i + 1));
-    //   [array[i], array[j]] = [array[j], array[i]];
-    // }
+    return array.sort(() => Math.random() - 0.5);
   }
 
   const randomizedCandidates = shuffleArray(candidates);
