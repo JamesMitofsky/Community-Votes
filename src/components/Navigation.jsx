6db4a678b204bffa9311e16890c28d20a2456caa
James Mitofsky <JamesMitofsky@gmail.com>
2022-09-04 19:55:47 +0200

    Add icon to header

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,24 @@
 import { AppBar, Toolbar, Typography } from "@mui/material";
 import { NavLink } from "react-router-dom";
+import HowToVoteIcon from "@mui/icons-material/HowToVote";
 
 export default function Navigation() {
   return (
     <AppBar position="static">
       <Toolbar>
-        <NavLink style={{ textDecoration: "none", color: "unset" }} to="/">
-          <Typography variant="h1">Community Votes</Typography>
+        <NavLink
+          style={{
+            textDecoration: "none",
+            color: "unset",
+            display: "flex",
+            alignItems: "center",
+          }}
+          to="/"
+        >
+          <HowToVoteIcon />
+          <Typography sx={{ marginLeft: 1, fontSize: 25 }}>
+            Community Votes
+          </Typography>
         </NavLink>
       </Toolbar>
     </AppBar>
